614e416dae93e8bd10220f00e4e6dc3d1788de0a
André Santos <afsdab@gmail.com>
2015-05-29 01:28:44 -0300

    Update MahanaPermissionChecker.php
    A small adjust for the code follow PSR-2

--- src/Jrmadsen67/MahanaPermissionChecker/MahanaPermissionChecker.php
+++ src/Jrmadsen67/MahanaPermissionChecker/MahanaPermissionChecker.php
@@ -3,7 +3,8 @@
 use Jrmadsen67\MahanaPermissionChecker\repositories\PermissionCheckerRepositoryInterface;
 
 
-class MahanaPermissionChecker {
+class MahanaPermissionChecker 
+{
 
 	protected $permission_repo;
 
@@ -12,36 +13,43 @@ class MahanaPermissionChecker {
 		$this->permission_repo = $permission_repo;
 	}
 
-
-	public function has_permission($action, $user, $object){
+	public function has_permission($action, $user, $object)
+	{
 		return $this->permission_repo->has_permission($action, $user, $object);
 	}
 	
-	public function check_permissions($action, $group_ids, $object){
+	public function check_permissions($action, $group_ids, $object)
+	{
 		return $this->permission_repo->check_permissions($action, $group_ids, $object);
 	}
 	
-	public function check_child_permissions($action, $group_ids, $object){
+	public function check_child_permissions($action, $group_ids, $object)
+	{
 		return $this->permission_repo->check_child_permissions($action, $group_ids, $object);
 	}
 	
-	public function get_object_registry_id($object_id, $object_type_id){
+	public function get_object_registry_id($object_id, $object_type_id)
+	{
 		return $this->permission_repo->get_object_registry_id($object_id, $object_type_id);
 	}
 	
-	public function get_object($object_id, $object_type_id){
+	public function get_object($object_id, $object_type_id)
+	{
 		return $this->permission_repo->get_object($object_id, $object_type_id);
 	}
 	
-	public function get_object_types(){
+	public function get_object_types()
+	{
 		return $this->permission_repo->get_object_types();
 	}
 	
-	public function create_group_action($data){
+	public function create_group_action($data)
+	{
 		return $this->permission_repo->create_group_action($data);
 	}
 
-	public function remove_group_action($ids){
+	public function remove_group_action($ids)
+	{
 		return $this->permission_repo->remove_group_action($ids);
 	}
 
